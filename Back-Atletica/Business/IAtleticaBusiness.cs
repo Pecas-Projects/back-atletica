be631877a1951d3c65022dba39cbb8eb8f49ce48
Ana Tartari <anatartari412@gmail.com>
2020-09-29 16:53:28 -0300

    Arquitetura de projeto

---
+++ Back-Atletica/Business/IAtleticaBusiness.cs
@@ -0,0 +1,17 @@
+﻿using Back_Atletica.Models;
+using Back_Atletica.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_Atletica.Business
+{
+    public interface IAtleticaBusiness
+    {
+        HttpRes Create(Atletica atletica);
+
+        HttpRes FindAll();
+        HttpRes FindById();
+    }
+}
---
+++ Back-Atletica/Business/Implementação/AtleticaBusinessImpl.cs
@@ -0,0 +1,27 @@
+﻿using Back_Atletica.Controllers;
+using Back_Atletica.Models;
+using Back_Atletica.Repository;
+using Back_Atletica.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_Atletica.Business.Implementação
+{
+    public class AtleticaBusinessImpl : IAtleticaBusiness
+    {
+
+        private IAtleticaRepository _AtleticaRepository;
+
+        public AtleticaBusinessImpl(IAtleticaRepository atleticaRepository)
+        {
+            _AtleticaRepository = atleticaRepository;
+        }
+
+        public HttpRes Create(Atletica atletica)
+        {
+            return _AtleticaRepository.Create(atletica);
+        }
+    }
+}
