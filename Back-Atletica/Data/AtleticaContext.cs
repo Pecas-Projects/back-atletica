185d33eeab0fa4c634705d4981dc197d29f3e262
AmadoMaria <mariantoniamado@hotmail.com>
2020-09-29 17:12:42 -0300

    Construção do método OnModelCreating no AtleticaContext

--- Back-Atletica/Data/AtleticaContext.cs
+++ Back-Atletica/Data/AtleticaContext.cs
@@ -7,24 +7,53 @@ using System.Threading.Tasks;
 
 namespace Back_Atletica.Data
 {
-    public class AtleticaContext :DbContext
+    public class AtleticaContext : DbContext
     {
+        public DbSet<Atleta> Atletas { get; set; }
+        public DbSet<AtletaModalidade> AtletaModalidades { get; set; }
+        public DbSet<AtletaModalidadeTimeEscalado> AtletaModalidadeTimesEscalados { get; set; }
+        public DbSet<Atletica> Atleticas { get; set; }
+        public DbSet<Campus> Campus { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Faculdade> Faculdades { get; set; }
+        public DbSet<Funcao> Funcoes { get; set; }
+        public DbSet<Imagem> Imagens { get; set; }
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Membro> Membros { get; set; }
+        public DbSet<Modalidade> Modalidades { get; set; }
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Publicacao> Publicacoes { get; set; }
+        public DbSet<SolicitacaoAtleta> SolicitacaoAtletas { get; set; }
+        public DbSet<TimeEscalado> TimeEscalados { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //string de conexão com o Postgree
             string connStr = Env.ConnString;
             optionsBuilder.UseNpgsql(connStr);
-        }
-
-        public DbSet<Atletica> Atletica { get; set; }
-
-        public void Start()
-        {
-            this.Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Atleta>()
+                .HasOne<Pessoa>(a => a.Pessoa);
+
+            //modelBuilder.Entity<AtletaModalidade>().HasKey(am => new { am.AtletaId, am.ModalidadeId });
+
+            //modelBuilder.Entity<AtletaModalidade>()
+            //    .HasOne<Atleta>(am => am.AtletaId)
+            //    .WithMany<Modalidade>(am => am.ModalidadeId);
+
+        }
+
+
+        public void Start()
+        {
+            this.Database.EnsureCreated();
         }
 
     }
