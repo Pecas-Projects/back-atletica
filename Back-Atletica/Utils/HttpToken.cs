a9e17587fa166611f9e694968fa3e8b3635d29c1
Ana Tartari <anatartari412@gmail.com>
2020-10-23 18:08:44 -0300

    Metodo de armazenamento de dados ao realizar login

---
+++ Back-Atletica/Utils/HttpToken.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Back_Atletica.Utils
+{
+    public class HttpToken
+    {
+
+        public static long GetUserId(HttpContext context)
+        {
+            var identity = context.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+
+            var id = claim[0].Value;
+
+            return Convert.ToInt32(id);
+        }
+
+        public static string GetTokenType(HttpContext context)
+        {
+            var identity = context.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+
+            string type = claim[2].Value;
+
+            return type;
+        }
+    }
+}
