bd25adbfc358fbd1072b50d29406105f9385687e
Davi Costa <davi.machado.costa@hotmail.com>
2020-10-27 12:16:09 -0300

    Criando relacionamento entre Membro e AtleticaModalidade

--- Back-Atletica/Data/AtleticaContext.cs
+++ Back-Atletica/Data/AtleticaContext.cs
@@ -449,10 +449,6 @@ namespace Back_Atletica.Data
                 .WithMany(m => m.AtleticaModalidades)
                 .HasForeignKey(am => am.ModalidadeId);
 
-            modelBuilder.Entity<AtleticaModalidade>()
-                .Property(p => p.NomeCoordenador)
-                .HasMaxLength(45);
-
             /* AtletaAtleticaModalidadeTimeEscalado*/
             modelBuilder.Entity<AtletaAtleticaModalidadeTimeEscalado>().HasKey(am => new { am.AtletaAtleticaModalidadeTimeEscaladoId });
 
---
+++ Back-Atletica/Migrations/20201027151334_RelacionamentoMembroAtleticaModalidade.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Back_Atletica.Migrations
+{
+    public partial class RelacionamentoMembroAtleticaModalidade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NomeCoordenador",
+                table: "AtleticaModalidades");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MembroId",
+                table: "AtleticaModalidades",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AtleticaModalidades_MembroId",
+                table: "AtleticaModalidades",
+                column: "MembroId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AtleticaModalidades_Membros_MembroId",
+                table: "AtleticaModalidades",
+                column: "MembroId",
+                principalTable: "Membros",
+                principalColumn: "MembroId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AtleticaModalidades_Membros_MembroId",
+                table: "AtleticaModalidades");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AtleticaModalidades_MembroId",
+                table: "AtleticaModalidades");
+
+            migrationBuilder.DropColumn(
+                name: "MembroId",
+                table: "AtleticaModalidades");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NomeCoordenador",
+                table: "AtleticaModalidades",
+                type: "character varying(45)",
+                maxLength: 45,
+                nullable: true);
+        }
+    }
+}
