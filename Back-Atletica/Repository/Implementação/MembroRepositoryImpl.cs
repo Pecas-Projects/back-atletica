c45814d1ce4d0554c975873518f4219642d0ecfa
fernandanlisboa <fernandanlisboa@gmail.com>
2020-11-11 11:12:38 -0300

    adicionando mensagem de atlética não encontrada na busca de todos os membros em uma atlética

--- Back-Atletica/Repository/Implementação/MembroRepositoryImpl.cs
+++ Back-Atletica/Repository/Implementação/MembroRepositoryImpl.cs
@@ -96,6 +96,13 @@ namespace Back_Atletica.Repository.Implementação
         {
             List<Membro> membros = new List<Membro>();
 
+            AtleticaRepositoryImpl atletica = new AtleticaRepositoryImpl(context);
+
+            if (!atletica.existeAtletica(atleticaId))
+            {
+                return new HttpRes(404, "Não existe nenhuma atlética com este id");
+            }
+
             membros = context.Membros.Include(a => a.Pessoa).Where(m => m.Pessoa.AtleticaId.Equals(atleticaId)).ToList<Membro>();
 
             return new HttpRes(200, membros);
