4318ae68c44914b6db5d9cc62ef9cfc905c24b0c
Ana Tartari <anatartari412@gmail.com>
2020-11-12 20:31:02 -0300

    Separação de RequestModels

---
+++ Back-Atletica/Utils/RequestModels/EnderecoModel.cs
@@ -0,0 +1,50 @@
+﻿using Back_Atletica.Models;
+
+namespace Back_Atletica.Utils.RequestModels
+{
+    public class EnderecoModel
+    {
+        public class CampusModel
+        {
+            public string Nome { get; set; }
+            public string Cidade { get; set; }
+            public string Bairro { get; set; }
+            public string Rua { get; set; }
+            public string Estado { get; set; }
+            public string CEP { get; set; }
+            public FaculdadeModel Faculdade { get; set; }
+
+            public Campus Transform()
+            {
+                Campus campos = new Campus
+                {
+                    Nome = Nome,
+                    Cidade = Cidade,
+                    Bairro = Bairro,
+                    Rua = Rua,
+                    Estado = Estado,
+                    CEP = CEP,
+                    Faculdade = Faculdade.Transform()
+                };
+
+                return campos;
+            }
+        }
+
+        public class FaculdadeModel
+        {
+            public string Nome { get; set; }
+
+            public Faculdade Transform()
+            {
+                Faculdade faculdade = new Faculdade
+                {
+                    Nome = this.Nome
+                };
+
+                return faculdade;
+            }
+
+        }
+    }
+}
---
+++ Back-Atletica/Utils/RequestModels/PessoaModel.cs
@@ -0,0 +1,34 @@
+﻿using Back_Atletica.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Back_Atletica.Utils.RequestModels
+{
+    public class PessoaModel
+    {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Sobrenome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Whatsapp { get; set; }
+        public string Tipo { get; set; }
+        public char Genero { get; set; }
+        public int CursoId { get; set; }
+
+        public Pessoa Transform()
+        {
+            Pessoa pessoa = new Pessoa
+            {
+                Nome = this.Nome,
+                Sobrenome = this.Sobrenome,
+                Email = Email,
+                Whatsapp = this.Whatsapp,
+                Tipo = this.Tipo,
+                Genero = this.Genero,
+                CursoId = this.CursoId
+            };
+            return pessoa;
+        }
+    }
+}
