4b8ebd8b667d23de87dcd415632aaa93e0f80a09
fernandanlisboa <fernandanlisboa@gmail.com>
2020-11-13 15:35:05 -0300

    Criação do Model auxiliar de Modalidade
    ModalidadeAtletica

---
+++ Back-Atletica/Utils/RequestModels/ModalidadeModel.cs
@@ -0,0 +1,34 @@
+﻿using Back_Atletica.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_Atletica.Utils.RequestModels
+{
+    public class ModalidadeModel
+    {
+
+        public class ModalidadeAtletica
+        {
+            [Required]
+            public int CoordenadorId { get; set; }
+            [Required]
+            public int ModalidadeId { get; set; }
+
+            public AtleticaModalidade Transform(int atleticaId)
+            {
+                AtleticaModalidade atleticaModalidade = new AtleticaModalidade
+                {
+                    ModalidadeId = this.ModalidadeId,
+                    MembroId = this.CoordenadorId,
+                    AtleticaId = atleticaId
+                };
+
+                return atleticaModalidade;
+            }
+
+        }
+    }
+}
