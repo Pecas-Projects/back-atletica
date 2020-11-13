521e761f471d284d1aa93b2c890101acd4d1ccc1
Davi Costa <davi.machado.costa@hotmail.com>
2020-11-13 10:17:24 -0300

    Criando model auxiliar de publicação

---
+++ Back-Atletica/Utils/RequestModels/PublicacaoModel.cs
@@ -0,0 +1,31 @@
+﻿using Back_Atletica.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_Atletica.Utils.RequestModels
+{
+    public class PublicacaoModel
+    {
+        [Required]
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        [Required]
+        public int AtleticaId { get; set; }
+        public int ImagemId { get; set; }
+        public Publicacao Transform()
+        {
+            Publicacao publicacao = new Publicacao
+            {
+                Titulo = Titulo,
+                Descricao = Descricao,
+                ImagemId = ImagemId,
+                AtleticaId = AtleticaId
+            };
+            return publicacao;
+
+        }
+    }
+}
