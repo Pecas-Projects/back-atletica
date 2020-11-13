277d562b77f37323e79ac1ade534f1caeffd6ab2
fernandanlisboa <fernandanlisboa@gmail.com>
2020-11-13 19:51:43 -0300

    Criação do ResponseModel de Modalidades
    - Model auxiliar do coordenador da modalidade
    - Model auxiliar de AtleticaModalidade

---
+++ Back-Atletica/Utils/ResponseModels/ModalidadeResponseModels.cs
@@ -0,0 +1,59 @@
+﻿using Back_Atletica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_Atletica.Utils.ResponseModels
+{
+    public class ModalidadeResponseModels
+    {
+        public class ModalidadesAtletica
+        {
+            public string Modalidade { get; set; }
+            public ImagemResponseModel ImagemModalidade { get; set; }
+            public string Coordenador { get; set; }
+            public List<AgendaTreino> AgendaTreinos { get; set; }
+
+            public List<ModalidadesAtletica> Transform(List<AtleticaModalidade> atleticaModalidade)
+            {
+
+                List<ModalidadesAtletica> ma = new List<ModalidadesAtletica>();
+
+                foreach(var a in atleticaModalidade)
+                {
+                    ImagemResponseModel img = new ImagemResponseModel();
+
+                    ModalidadesAtletica m = new ModalidadesAtletica
+                    {
+                        Modalidade = a.Modalidade.Nome,
+                        ImagemModalidade = a.Imagem != null ? img.Transform(a.Imagem) : null,
+                        Coordenador = a.Membro.Pessoa.Nome,
+                        AgendaTreinos = new List<AgendaTreino>(a.AgendaTreinos)
+                    };
+
+                    ma.Add(m);
+                }
+
+                return ma;
+            }
+
+        }
+
+        public class Coordenador
+        {
+            public string Nome { get; set; }
+
+            public Coordenador Transform(Pessoa pessoa)
+            {
+                Coordenador membro = new Coordenador
+                {
+                    Nome = pessoa.Nome
+                };
+
+                return membro;
+            }
+        }
+
+    }
+}
