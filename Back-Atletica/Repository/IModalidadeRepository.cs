f1585b1116599139b03357197acef1d9728594b5
fernandanlisboa <fernandanlisboa@gmail.com>
2020-11-14 18:05:54 -0300

    Adição do método de excluir modalidade na atlética à interface do repositório de Modalidade

--- Back-Atletica/Repository/IModalidadeRepository.cs
+++ Back-Atletica/Repository/IModalidadeRepository.cs
@@ -15,6 +15,7 @@ namespace Back_Atletica.Repository
         HttpRes BuscarTodosNaAtletica(int atleticaId);
         HttpRes BuscarPorId(int id);
         HttpRes Deletar(int id);
+        HttpRes ExcluiModalidade(int atleticaId, int modalidadeId);
         bool existeModalidade(int id);
     }
 }
