e5dc04a293a9f1914276507d1d4325c01cea92bb
Davi Costa <davi.machado.costa@hotmail.com>
2020-11-20 16:26:04 -0300

    Criando estrutura do CRUD de faculdade

--- Back-Atletica/Env.cs
+++ Back-Atletica/Env.cs
@@ -8,13 +8,13 @@ namespace Back_Atletica
     public class Env
     {
         //Maria string
-        // public static string ConnString = "User ID = postgres; Password=0; Host=localhost;Port=5432; Database=Olympos-db;";
+        //public static string ConnString = "User ID = postgres; Password=0; Host=localhost;Port=5432; Database=Olympos-db;";
         //public static string ConnString = "User ID = postgres; Password=pass; Host=localhost;Port=5000; Database=Olympos-db;";
         //public static string ConnString = "User ID = postgres; Password=0; Host=localhost;Port=5432; Database=Olympos-db;";
         //Davi string
-        //public static string ConnString = "User ID = postgres; Password=password; Host=localhost;Port=5432; Database=Olympos;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=100;";
+        public static string ConnString = "User ID = postgres; Password=password; Host=localhost;Port=5432; Database=Olympos;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=100;";
         //Atari string
-        public static string ConnString = "User ID = postgres; Password=pass; Host=localhost;Port=5000; Database=Olympos-db;";
+        //public static string ConnString = "User ID = postgres; Password=pass; Host=localhost;Port=5000; Database=Olympos-db;";
         //public static string ConnString = "Database=db5h3tu260ofsh; Host=ec2-18-235-20-228.compute-1.amazonaws.com; Port=5432; User ID=htqmtyzoggbpnu; Password=74fde93835f7a6dfec243887db269383909f5f95a24373e4f53fa7e727751e64;";
 
         public static string Secret = "06345633691ed389ac8cda3568b0b174";
