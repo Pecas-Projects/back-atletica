9a70faf3a16685e7f0ebf89c24209e89cb1b1dbe
AmadoMaria <mariantoniamado@hotmail.com>
2020-11-28 21:29:47 -0300

    Update Env.cs

--- Back-Atletica/Env.cs
+++ Back-Atletica/Env.cs
@@ -8,14 +8,14 @@ namespace Back_Atletica
     public class Env
     {
         //Maria string
-        public static string ConnString = "User ID = postgres; Password=0; Host=localhost;Port=5432; Database=Olympos-db;";
+        //public static string ConnString = "User ID = postgres; Password=0; Host=localhost;Port=5432; Database=Olympos-db;";
         //public static string ConnString = "User ID = postgres; Password=pass; Host=localhost;Port=5000; Database=Olympos-db;";
         //public static string ConnString = "User ID = postgres; Password=0; Host=localhost;Port=5432; Database=Olympos-db;";
         //Davi string
         // public static string ConnString = "User ID = postgres; Password=password; Host=localhost;Port=5432; Database=Olympos;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=100;";
         //Atari string
         //public static string ConnString = "User ID = postgres; Password=pass; Host=localhost;Port=5000; Database=Olympos-db;";
-        //public static string ConnString = "Database=d997bj0duk47mj; Host=ec2-54-166-114-48.compute-1.amazonaws.com; Port=5432; User ID=hwcxfgdafwshil; Password=5117528ea4aa31db3956b05641b7dea22967153b1d94aaac1ec00d2b60f84d69;";
+        public static string ConnString = "Database=d997bj0duk47mj; Host=ec2-54-166-114-48.compute-1.amazonaws.com; Port=5432; User ID=hwcxfgdafwshil; Password=5117528ea4aa31db3956b05641b7dea22967153b1d94aaac1ec00d2b60f84d69;";
 
 
         public static string Secret = "06345633691ed389ac8cda3568b0b174";
