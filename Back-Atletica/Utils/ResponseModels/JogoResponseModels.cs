8bf9b9c29ecc3354512c63f9a3451302feb0c64d
Davi Costa <davi.machado.costa@hotmail.com>
2020-11-29 19:26:14 -0300

    Consertando método de buscar jogos por modalidade

--- Back-Atletica/Utils/ResponseModels/JogoResponseModels.cs
+++ Back-Atletica/Utils/ResponseModels/JogoResponseModels.cs
@@ -8,23 +8,13 @@ namespace Back_Atletica.Utils.ResponseModels
 {
     public class JogoResponseModels
     {
-        public JogoResponseModels()
-        {
-            Atleticas = new List<AtleticaJogoModel>();
-            Atletas = new List<AtletaJogoModel>();
-        }
-
         public int JogoId { get; set; }
         public DateTime DataHora { get; set; }
-        public List<AtleticaJogoModel> Atleticas { get; set; }
-        public List<AtletaJogoModel> Atletas { get; set; }
+        public List<TimeResponseModel> Times { get; set; }
 
-        public class AtleticaJogoModel
+        public JogoResponseModels()
         {
-            public int AtleticaId { get; set; }
-            public string Nome { get; set; }
-            public int? Pontos { get; set; }
+            Times = new List<TimeResponseModel>();
         }
-
     }
 }
---
+++ Back-Atletica/Utils/ResponseModels/TimeResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_Atletica.Utils.ResponseModels
+{
+    public class TimeResponseModel
+    {
+        public int TimeId { get; set; }
+        public int AtleticaId { get; set; }
+        public string Nome { get; set; }
+        public int? Pontos { get; set; }
+        public List<AtletaJogoModel> Atletas { get; set; }
+
+        public TimeResponseModel()
+        {
+            Atletas = new List<AtletaJogoModel>();
+        }
+    }
+}
