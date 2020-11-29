6f0b3da4b68ea4141184dab4af6c81a9af4ca92a
Ana Tartari <anatartari412@gmail.com>
2020-11-29 17:18:15 -0300

    CHaves estrangeiras em update de atletica

--- Back-Atletica/Repository/Implementação/AtleticaRepositoryImpl.cs
+++ Back-Atletica/Repository/Implementação/AtleticaRepositoryImpl.cs
@@ -44,7 +44,9 @@ namespace Back_Atletica.Repository.Implementação
                 atletica.PIN = atleticaDados.PIN;
                 atletica.Senha = atleticaDados.Senha;
                 atletica.CampusId = atleticaDados.CampusId;
-                atletica.Campus.FaculdadeId = atletica.Campus.FaculdadeId;
+                atletica.Campus.CampusId = atleticaDados.Campus.CampusId;
+                atletica.Campus.FaculdadeId = atleticaDados.Campus.FaculdadeId;
+                atletica.Campus.Faculdade.FaculdadeId = atleticaDados.Campus.Faculdade.FaculdadeId;
 
                 _context.Entry(atleticaDados).CurrentValues.SetValues(atletica);
                 _context.Entry(atleticaDados.Campus).CurrentValues.SetValues(atletica.Campus);
